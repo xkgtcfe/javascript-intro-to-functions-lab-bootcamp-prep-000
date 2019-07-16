b94d589767c8bf1525e8126a7386b253dfcc89f1
Carolina Ferreira <carolina.ferreira.16@ucl.ac.uk>
2019-07-16 03:12:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function sayHiToGrandma(string) {
   if (string === lowercase) {
     return "I can\'t hear you!"
   } else if (string === uppercase) {
-    return "I can\'t hear you!"
-  }
+    return "YES INDEED!"
+  } else if
 }
