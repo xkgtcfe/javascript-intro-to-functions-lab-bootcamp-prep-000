743f6775a3e108c4b14c8bb47d9dec75e9f2106e
Carolina Ferreira <carolina.ferreira.16@ucl.ac.uk>
2019-07-16 03:03:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,17 +16,12 @@ function logWhisper(string) {
 }
 
 
+ 
+function sayHiToGrandma(string) {
+  var lowercase = string.toLowerCase() 
+  if (string === "lowercase") {
+     
+  }
 
-function sayHiToGranda(string) {
-  var lowercase = string.toLowerCase()
-  if (string === "lowercase")
-  return "I can't hear you"
-  else 
-  var uppercase = string.toUpperCase
-  if (string === "uppercase")
-  return "YES INDEED!"
-  else
-  if (string = "I love you, Grandma.")
-  return "I love you too."
-
+ 
 }
